30fbcc3ec3c2851028e092eacefec15e0c11bd5b
Arzu Akkus <ilaydaarzuakkus@gmail.com>
2025-06-22 00:04:49 +0300

    ci: github actions için sqlite fallback eklendi

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345@host.docker.internal/large-blog"
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # CI/CD ortamı için
+    else:
+        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345@host.docker.internal/large-blog"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "gizli-bir-key"
 
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"  # Çok önemli! Cross-origin çağrılarda "None" da olabilir
-    SESSION_COOKIE_SECURE = False  # Sadece HTTPS'te True olmalı
+    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = False
