97f1f7d5015065cc7238033d9c25f69aaf854bc7
Arzu Akkus <ilaydaarzuakkus@gmail.com>
2025-06-22 13:57:39 +0300

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM python:3.12-slim
+
 WORKDIR /app
+
 COPY . .
+
 RUN pip install --no-cache-dir -r requirements.txt
+
 EXPOSE 5000
-CMD ["python", "run.py"]
+
+CMD ["gunicorn", "run:app", "--bind", "0.0.0.0:5000"]
--- config.py
+++ config.py
@@ -2,21 +2,28 @@ import os
 import redis
 
 class Config:
-    if os.environ.get("GITHUB_ACTIONS") == "true":
-        SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # CI/CD ortamı için
+    GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS", "false") == "true"
+
+    if GITHUB_ACTIONS:
+        SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
         REDIS_HOST = "localhost"
     else:
-        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345@host.docker.internal/large-blog"
-        REDIS_HOST = "redis"
+        DB_USER = os.getenv("DB_USER")
+        DB_PASS = os.getenv("DB_PASS")
+        DB_HOST = os.getenv("DB_HOST")
+        DB_NAME = os.getenv("DB_NAME")
+
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+        REDIS_HOST = os.getenv("REDIS_HOST")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "gizli-bir-key"
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-key")
 
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"
     SESSION_COOKIE_SECURE = False
 
-# Redis bağlantısı (CI/CD ve Compose uyumlu)
+# Redis bağlantısı
 redis_client = redis.Redis(
     host=Config.REDIS_HOST,
     port=6379,
